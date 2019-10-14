3afdf1a6804710a51765a86f30eabaa3c2d8d055
Diego Sepulveda <diego.felipe.sepulveda.briones@gmail.com>
2019-10-14 12:19:42 -0300

    Firts commit

--- sensor-producer.py
+++ sensor-producer.py
@@ -4,7 +4,7 @@ import sys
 import json
 from collections import OrderedDict
 from miflora.miflora_poller import MiFloraPoller, MI_BATTERY, MI_CONDUCTIVITY, MI_LIGHT, MI_MOISTURE, MI_TEMPERATURE
-from btlewrap import available_backends, BluepyBackend, GatttoolBackend, PygattBackend, BluetoothBackendException
+from btlewrap import available_backends, BluepyBackend, BluetoothBackendException
 from time import time, sleep, localtime, strftime
 from colorama import Fore, Back, Style
 from colorama import init as colorama_init
@@ -40,7 +40,7 @@ def all_procedure(mac=default_mac, delay=default_delay, device_name=default_devi
     topic = topic
 
     flora = dict()
-    flora_poller = MiFloraPoller(mac=mac, backend=GatttoolBackend, cache_timeout=miflora_cache_timeout, retries=3, adapter=used_adapter)
+    flora_poller = MiFloraPoller(mac=mac, backend=BluepyBackend, cache_timeout=miflora_cache_timeout, retries=3, adapter=used_adapter)
     flora['poller'] = flora_poller
     flora['name_pretty'] = device_name
     flora['mac'] = flora_poller._mac
