4a4aa7c0d2fe5f14f18cf829459ac7fc0c410513
Diego Sepulveda <diego.felipe.sepulveda.briones@gmail.com>
2019-10-14 13:55:40 -0300

    Firts commit

--- bluetooth-scan.py
+++ bluetooth-scan.py
@@ -22,31 +22,31 @@ msgs=[]
 
 for address, name in list(devices.items()):
     try:	
-	if name == "Flower mate" or name == "Flower care":
-		topic= baseTopic + address.replace(':', '') + '/'
-		requester = GATTRequester(address, True)
-		#Read battery and firmware version attribute
-		data=requester.read_by_handle(0x0038)[0]
-		battery, firmware = unpack('<xB5s',data)
-		msgs.append({'topic': topic + 'battery', 'payload':battery})
-		msgs.append({'topic': topic + 'firmware', 'payload':firmware})
-		#Enable real-time data reading
-		requester.write_by_handle(0x0033, str(bytearray([0xa0, 0x1f])))
+	    if name == "Flower mate" or name == "Flower care":
+		    topic= baseTopic + address.replace(':', '') + '/'
+		    requester = GATTRequester(address, True)
+		    #Read battery and firmware version attribute
+		    data=requester.read_by_handle(0x0038)[0]
+		    battery, firmware = unpack('<xB5s',data)
+		    msgs.append({'topic': topic + 'battery', 'payload':battery})
+		    msgs.append({'topic': topic + 'firmware', 'payload':firmware})
+		    #Enable real-time data reading
+		    requester.write_by_handle(0x0033, str(bytearray([0xa0, 0x1f])))
 		#Read plant data
-		data=requester.read_by_handle(0x0035)[0]
-		temperature, sunlight, moisture, fertility = unpack('<hxIBHxxxxxx',data)
-		msgs.append({'topic': topic + 'temperature', 'payload':temperature/10.})
-		msgs.append({'topic': topic + 'sunlight', 'payload':sunlight})
-		msgs.append({'topic': topic + 'moisture', 'payload':moisture})
-		msgs.append({'topic': topic + 'fertility', 'payload':fertility})
-		if (verbose):
-			print("name: {}, address: {}".format(name, address))
-			print("Battery level:",battery,"%")
-			print ("Firmware version:",firmware)
-			print ("Light intensity:",sunlight,"lux")
-			print ("Temperature:",temperature/10.," C")
-			print ("Soil moisture:",moisture,"%")
-			print ("Soil fertility:",fertility,"uS/cm")
+		    data=requester.read_by_handle(0x0035)[0]
+		    temperature, sunlight, moisture, fertility = unpack('<hxIBHxxxxxx',data)
+		    msgs.append({'topic': topic + 'temperature', 'payload':temperature/10.})
+		    msgs.append({'topic': topic + 'sunlight', 'payload':sunlight})
+		    msgs.append({'topic': topic + 'moisture', 'payload':moisture})
+		    msgs.append({'topic': topic + 'fertility', 'payload':fertility})
+		    if (verbose):
+			    print("name: {}, address: {}".format(name, address))
+			    print("Battery level:",battery,"%")
+			    print ("Firmware version:",firmware)
+			    print ("Light intensity:",sunlight,"lux")
+			    print ("Temperature:",temperature/10.," C")
+			    print ("Soil moisture:",moisture,"%")
+			    print ("Soil fertility:",fertility,"uS/cm")
     except:
         print ("Error during reading:", sys.exc_info()[0])
 
